f7cf4dec43184ccc5693e9ffdb72374071b77946
Utku Sever <c.utkusever@outlook.com>
2025-04-03 16:13:24 +0300

    Add: SOLID, I with 2 examples

---
+++ Assets/Scripts/SOLID/InterfaceSegregation/InterfaceSegregationExample2.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceSegregationExample2 : MonoBehaviour
+{
+    private void Start()
+    {
+        IWeapon sword = new Sword();
+        IWeapon bow = new Bow();
+        List<IWeapon> weapons = new List<IWeapon> { sword, bow };
+        List<IReloadable> reloadables = new List<IReloadable> { new Bow() };
+        foreach (var weapon in weapons)
+        {
+            weapon.Attack();
+        }
+
+        foreach (var reloadable in reloadables)
+        {
+            reloadable.Reload();
+        }
+    }
+}
+
+public interface IWeapon
+{
+    void Attack();
+    //void Reload(); what about a sword?
+}
+
+public interface IReloadable
+{
+    void Reload();
+}
+
+public interface IMeleeWeapon : IWeapon
+{
+    void Swing();
+}
+
+public interface IRangedWeapon : IWeapon, IReloadable
+{
+    void Shoot();
+}
+
+public class Sword : IMeleeWeapon
+{
+    public void Attack()
+    {
+        Swing();
+    }
+
+    public void Swing()
+    {
+        Debug.Log("Swinging the sword!");
+    }
+}
+
+public class Bow : IRangedWeapon
+{
+    public void Attack()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("Shooting an arrow!");
+    }
+
+    public void Reload()
+    {
+        Debug.Log("Reloading the bow!");
+    }
+}
